15587853427aed33011d35b38e0e9b42ab93a282
yeop <woco11@naver.com>
2024-04-04 20:40:13 +0900

    DB관계형

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment, User, Profile
+from .models import Post, Comment, User, Profile,Tag
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(Tag)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,12 +5,13 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
-    
+    tag = models.ManyToManyField('Tag', null=True, blank=True)
+
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
     author = models.CharField(max_length=20)
     message = models.TextField()
     created = models.DateField(auto_now_add=True)
@@ -26,3 +27,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
